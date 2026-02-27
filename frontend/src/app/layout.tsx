71301f2c6821485821b0ffcbaa02a92c667ab0e7
Aman Kumar <aman.hooda10001@gmail.com>
2026-02-27 10:56:02 +0530

    revert: restore original mobile UI baseline from commit 6898d25

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
-import { MobileBottomNav } from "@/components/MobileBottomNav";
 import { ThemeProvider } from "@/components/theme-provider";
 import { BrandingProvider } from "@/context/ThemeContext";
 import { SocketProvider } from "@/context/SocketContext";
@@ -77,11 +76,10 @@ export default function RootLayout({
           <BrandingProvider>
             <SocketProvider>
               <Navbar />
-              <main className="min-h-screen flex flex-col pb-20 md:pb-0">
+              <main className="min-h-screen flex flex-col">
                 {children}
               </main>
               <Footer />
-              <MobileBottomNav />
             </SocketProvider>
           </BrandingProvider>
         </ThemeProvider>
