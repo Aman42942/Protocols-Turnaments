3e5f52fca635554ab73649751112cc5a426d36a2
Aman Kumar <aman.hooda10001@gmail.com>
2026-03-06 01:42:25 +0530

    fix: SecurityService dependency injection

--- backend/src/security/security.module.ts
+++ backend/src/security/security.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SecurityService } from './security.service';
 import { SecurityController } from './security.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+    imports: [PrismaModule, NotificationsModule],
     providers: [SecurityService],
     controllers: [SecurityController],
     exports: [SecurityService],
